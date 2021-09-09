1fb74e675e1378a510df011b74e3df9d4435ff78
yefimkh <michael.khaikin@gmail.com>
2021-09-09 14:23:24 -0400

    No stringify req

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/posts', (req, res) =>{
 })
 
 app.post('/posts', (req, res) => {
-    console.log("I got a Post: " + JSON.stringify(req))
+    console.log("I got a Post: ")
     res.send("Thanks for image...")
 })
 
