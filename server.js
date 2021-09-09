6972cc55a2be9a1c94c6e104e43edb2f7305b6c7
yefimkh <michael.khaikin@gmail.com>
2021-09-09 11:54:52 -0400

    Enable All CORS Requests

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ var cors = require('cors')
 require("dotenv").config()
 
 const app = express()
+/*
 var corsOptions = {
     origin: 'http://localhost:3000',
     optionsSuccessStatus: 200 
   }
 app.use(cors(corsOptions))
-
+*/
+app.use(cors())
 app.get('/posts', (req, res) =>{
     res.send("Get any information")
 })
